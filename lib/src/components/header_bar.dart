b91facd697bbb536dee592ad2e438867ac153f4b
Polabiel <bielgabrieloliveira77@gmail.com>
2024-07-24 20:37:32 -0300

    feat: Add menu and notifications buttons to HeaderBar component

--- lib/src/components/header_bar.dart
+++ lib/src/components/header_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gohealth/src/app/home/side_menu.dart';
 
 class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
   const HeaderBar({super.key});
@@ -7,19 +8,8 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Theme.of(context).primaryColor,
-      toolbarHeight: 85.0,
       title: Row(
         children: <Widget>[
-          IconButton(
-            iconSize: 40.0,
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-              // Função para abrir menu
-            },
-            style: ButtonStyle(
-              foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-            ),
-          ),
           const Expanded(
             child: Text(
               'Olá, Gabriel Oliveira',
