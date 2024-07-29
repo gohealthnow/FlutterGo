dc926c531c4303e5d3e3e93a51558a077ef93c91
Polabiel <bielgabrieloliveira77@gmail.com>
2024-07-29 00:34:13 -0300

    feat: Add login form to LoginPage

--- lib/src/app/login_page.dart
+++ lib/src/app/login_page.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:gohealth/src/components/button_field.dart';
 import 'package:gohealth/src/components/custom_input_field.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,15 +26,101 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 50),
               const FlutterLogo(size: 100),
               const SizedBox(height: 50),
-              const CustomInputField(
-                labelText: 'Email',
-                hintText: 'jonh_doe@gohealth.com',
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'E-mail',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (email) {
+                      if (email!.isEmpty) {
+                        return 'Please enter your email';
+                      } else if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(_emailController.text)) {
+                        return 'Por favor, digite um e-mail correto';
+                      }
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your email',
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 0.0,
+                        horizontal: 10.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 20),
-              const CustomInputField(
-                labelText: 'Senha',
-                hintText: 'pao_sem_ceu',
-                obscureText: true,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Password',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (password) {
+                      if (password!.isEmpty) {
+                        return 'Please enter your password';
+                      } else if (password.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your password',
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 0.0,
+                        horizontal: 10.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -38,12 +133,12 @@ class LoginPage extends StatelessWidget {
                       foregroundColor: const Color.fromRGBO(
                           0, 90, 226, 0.85), // Cor do texto
                     ),
-                    child: const Text('Esqueceu a senha?'),
+                    child: const Text('Forget your password?'),
                   ),
                 ],
               ),
               const SizedBox(height: 40),
-              const ButtonField(textButton: "Entrar"),
+              const ButtonField(textButton: "Login"),
               const SizedBox(height: 20),
               const SizedBox(height: 20),
               const Divider(),
@@ -52,7 +147,7 @@ class LoginPage extends StatelessWidget {
                 onPressed: () {
                   // Implementar ação de cadastro
                 },
-                child: const Text('Você não se cadastrou? Clique aqui'),
+                child: const Text('Create an account'),
               ),
             ],
           ),
