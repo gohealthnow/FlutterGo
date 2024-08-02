0833409a7d6cb5ed8a36fa978a148c739e1e0714
Gabriel <bielgabrieloliveira77@gmail.com>
2024-08-01 22:06:04 -0300

    feat: Refactor IUser interface to return Future<bool> for checkToken method

--- lib/api/interfaces/user_interface.dart
+++ lib/api/interfaces/user_interface.dart
@@ -3,5 +3,5 @@ import 'package:gohealth/api/interfaces/local_storage_interface.dart';
 abstract class IUser extends ILocalStorage {
   Future<dynamic> authenticate(String email, String password);
   Future<dynamic> registerUser(String email, String name, String password);
-  bool checkToken();
+  Future<bool> checkToken();
 }
