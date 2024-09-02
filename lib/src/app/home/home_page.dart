1365b71de854298449d765cb756e5133728a81a7
Polabiel <bielgabrieloliveira77@gmail.com>
2024-09-02 00:32:11 -0300

    feat: Refactor PharmacyComponent to include lazy loading for images
    The PharmacyComponent has been refactored to include lazy loading for images. This improves the page load performance by loading images only when they are visible to the user. Additionally, the code has been updated to use GestureDetector for navigation to the pharmacy page based on the ID.

--- lib/src/app/home/home_page.dart
+++ lib/src/app/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gohealth/src/components/Pharmacy/pharmacy.dart';
+import 'package:gohealth/src/components/banner.dart';
+import 'package:gohealth/src/components/pharmacy.dart';
 import 'package:gohealth/src/components/side_menu.dart';
 import 'package:gohealth/src/components/header_bar.dart';
 
@@ -13,14 +14,37 @@ class Homepage extends StatefulWidget {
 class HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: HeaderBarState(),
-      drawer: SideMenu(),
+    return Scaffold(
+      appBar: const HeaderBarState(),
+      drawer: const SideMenu(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            PharmacyComponentState(),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Farmácias perto de você',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 100),
+              child: const PharmacyComponentState(),
+            ),
+            const Divider(),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Farmácias perto de você',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 500),
+              child: const BannerComponent(),
+            ),
+            const Divider(),
           ],
         ),
       ),
---
+++ lib/src/components/banner.dart
@@ -0,0 +1,86 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:gohealth/api/layout/product_view_model.dart';
+import 'package:gohealth/api/models/product_models.dart';
+import 'package:gohealth/api/repositories/product_repository.dart';
+
+class BannerComponent extends StatefulWidget {
+  const BannerComponent({super.key});
+
+  @override
+  _BannerComponentState createState() => _BannerComponentState();
+}
+
+class _BannerComponentState extends State<BannerComponent> {
+  final _repository = ProductRepository();
+  final _viewModel = ProductsViewModel(ProductRepository());
+
+  Future<List<ProductModels>>? products;
+
+  @override
+  void initState() {
+    super.initState();
+    products = _viewModel.loadProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: FutureBuilder<List<ProductModels>>(
+            future: products,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('Erro: ${snapshot.error}');
+              } else {
+                if (snapshot.data!.isEmpty) {
+                  return Container();
+                } else {
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.primaries[
+                                  Random().nextInt(Colors.primaries.length)],
+                              radius: 50,
+                              child: Text(
+                                snapshot.data![index].name!,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              snapshot.data![index].price!.toString(),
+                              style: const TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
