1d1a10440b6de5d1c6e0e01790349884c9be771d
Polabiel <bielgabrieloliveira77@gmail.com>
2024-09-25 12:59:11 -0300

    Adicionar modelo PharmacyStockItem para gerenciar itens de estoque de farmácias

---
+++ lib/api/models/pharmacy_to_product_model.dart
@@ -0,0 +1,24 @@
+class PharmacyStockItem {
+  int? id;
+  int? pharmacyId;
+  int? productId;
+  int? quantity;
+
+  PharmacyStockItem({this.id, this.pharmacyId, this.productId, this.quantity});
+
+  PharmacyStockItem.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    pharmacyId = json['pharmacyId'];
+    productId = json['productId'];
+    quantity = json['quantity'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['pharmacyId'] = this.pharmacyId;
+    data['productId'] = this.productId;
+    data['quantity'] = this.quantity;
+    return data;
+  }
+}
