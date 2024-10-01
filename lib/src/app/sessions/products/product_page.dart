17c42a452864007db50aff5f15efd85331b9b355
Polabiel <bielgabrieloliveira77@gmail.com>
2024-09-30 22:10:55 -0300

    Adicionar funcionalidade para selecionar farmácias ao adicionar produtos ao carrinho

--- lib/src/app/sessions/products/product_page.dart
+++ lib/src/app/sessions/products/product_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gohealth/api/models/pharmacy_model.dart';
 import 'package:gohealth/api/models/product_models.dart';
+import 'package:gohealth/api/repositories/pharmacy_repository.dart';
 import 'package:gohealth/api/services/shared_local_storage_service.dart';
 import 'image_carousel.dart';
 import 'product_details.dart';
@@ -15,9 +17,26 @@ class ProductPage extends StatefulWidget {
 }
 
 class ProductState extends State<ProductPage> {
+
+  final _repository = PharmacyRepository();
+
+  List<PharmacyModels> pharmacies = [];
+
+
+  Future<List<PharmacyModels>> _fetchPharmacies() async {
+    for (var _ in widget.productModels.pharmacyProduct!) {
+      PharmacyModels pharmacy = await _repository.getPharmacyById(widget.productModels.id!);
+      pharmacies.add(pharmacy);
+    }
+    return pharmacies;
+  }
+
   @override
   void initState() {
     super.initState();
+    setState(() async {
+      await _fetchPharmacies();
+    });
   }
 
   @override
@@ -46,17 +65,69 @@ class ProductState extends State<ProductPage> {
         padding: const EdgeInsets.all(16.0),
         child: ElevatedButton(
           onPressed: () {
-            // Salvar os dados do produto no carrinho atráves do storage
-            SharedLocalStorageService().saveProduct(widget.productModels);
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Produto adicionado ao carrinho'),
-                backgroundColor: Colors.green,
-                duration: Duration(seconds: 2),
-              ),
-            );
-            Navigator.pop(context);
-          },
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return FutureBuilder<List<PharmacyModels>>(
+                    future: _fetchPharmacies(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return AlertDialog(
+                          title: Text('Erro'),
+                          content: Text('Erro ao carregar farmácias: ${snapshot.error}'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Fechar'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      } else {
+                        return AlertDialog(
+                          title: Text('Escolha uma farmácia'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: snapshot.data!.map((pharmacy) {
+                                return ListTile(
+                                  title: Text(pharmacy.name!),
+                                  subtitle: Text('Telefone: ${pharmacy.phone}'),
+                                  onTap: () {
+                                    // Ação ao selecionar a farmácia
+                                    Navigator.of(context).pop();
+                                    // Adicione aqui a lógica para prosseguir com a compra na farmácia selecionada
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('Produto adicionado ao carrinho na farmácia ${pharmacy.name}'),
+                                      ),
+                                    );
+                                    SharedLocalStorageService().addProductToCart(
+                                      product: widget.productModels,
+                                      pharmacy: pharmacy,
+                                    );
+                                  },
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Cancelar'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      }
+                    },
+                  );
+                },
+              );
+            },
           child: Text('Adicionar ao carrinho'),
           style: ElevatedButton.styleFrom(
             padding: EdgeInsets.symmetric(vertical: 16),
