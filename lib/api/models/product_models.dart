4854d2cac7d4e4f33b9d6e69dccd8d699814111f
Polabiel <bielgabrieloliveira77@gmail.com>
2024-10-05 01:09:12 -0300

    Refatorar modelos de produto e usuário para incluir categorias, avaliações e pedidos

--- lib/api/models/product_models.dart
+++ lib/api/models/product_models.dart
@@ -14,10 +14,8 @@ class ProductModels {
   String? updatedAt;
   List<UserModels>? user;
   List<PharmacyStockItem>? pharmacyProduct;
-  List<Null>? reviews;
-  List<Null>? categories;
-
-  int? quantity;
+  List<Review>? reviews;
+  List<String>? categories;
 
   ProductModels(
       {this.id,
@@ -35,50 +33,135 @@ class ProductModels {
       this.reviews,
       this.categories});
 
-  ProductModels.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    description = json['description'];
-    price = json['price'];
-    image = json['image'];
-    weight = json['weight'];
-    dimensions = json['dimensions'];
-    rating = json['rating'];
-    createdAt = json['createdAt'];
-    updatedAt = json['updatedAt'];
-    if (json['user'] != null) {
-      user = <UserModels>[];
-      json['user'].forEach((v) {
-        user!.add(new UserModels.fromJson(v));
-      });
-    }
-    if (json['PharmacyProduct'] != null) {
-      pharmacyProduct = <PharmacyStockItem>[];
-      json['PharmacyProduct'].forEach((v) {
-        pharmacyProduct!.add(new PharmacyStockItem.fromJson(v));
-      });
-    }
+  factory ProductModels.fromJson(Map<String, dynamic> json) {
+    return ProductModels(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      price: json['price'],
+      image: json['image'],
+      weight: json['weight'],
+      dimensions: json['dimensions'],
+      rating: json['rating'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+      categories: json['categories'] != null
+          ? (json['categories'] as List).map((i) => i.toString()).toList()
+          : null,
+      user: json['user'] != null
+          ? (json['user'] as List).map((i) => UserModels.fromJson(i)).toList()
+          : null,
+      pharmacyProduct: json['PharmacyProduct'] != null
+          ? (json['PharmacyProduct'] as List)
+              .map((i) => PharmacyStockItem.fromJson(i))
+              .toList()
+          : null,
+      reviews: json['review'] != null
+          ? (json['review'] as List).map((i) => Review.fromJson(i)).toList()
+          : null,
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['description'] = this.description;
-    data['price'] = this.price;
-    data['image'] = this.image;
-    data['weight'] = this.weight;
-    data['dimensions'] = this.dimensions;
-    data['rating'] = this.rating;
-    data['createdAt'] = this.createdAt;
-    data['updatedAt'] = this.updatedAt;
-    if (this.user != null) {
-      data['user'] = this.user!.map((v) => v.toJson()).toList();
-    }
-    if (this.pharmacyProduct != null) {
-      data['PharmacyProduct'] =
-          this.pharmacyProduct!.map((v) => v.toJson()).toList();
-    }
-    return data;
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'image': image,
+      'weight': weight,
+      'dimensions': dimensions,
+      'rating': rating,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'user': user?.map((v) => v.toJson()).toList(),
+      'PharmacyProduct': pharmacyProduct?.map((v) => v.toJson()).toList(),
+      'review': reviews?.map((v) => v.toJson()).toList(),
+    };
+  }
+}
+
+class Order {
+  String id;
+  int userId;
+  int productId;
+  int quantity;
+  DateTime createdAt;
+  DateTime updatedAt;
+  ProductModels product;
+  UserModels user;
+
+  Order({
+    required this.id,
+    required this.userId,
+    required this.productId,
+    required this.quantity,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.product,
+    required this.user,
+  });
+
+  factory Order.fromJson(Map<String, dynamic> json) {
+    return Order(
+      id: json['id'],
+      userId: json['userId'],
+      productId: json['productId'],
+      quantity: json['quantity'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+      product: ProductModels.fromJson(json['product']),
+      user: UserModels.fromJson(json['user']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userId': userId,
+      'productId': productId,
+      'quantity': quantity,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'product': product.toJson(),
+      'user': user.toJson(),
+    };
+  }
+}
+
+class Review {
+  int id;
+  DateTime createdAt;
+  DateTime updatedAt;
+  String title;
+  String body;
+  double rating;
+
+  Review({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.title,
+    required this.body,
+    required this.rating,
+  });
+
+  Review.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        createdAt = DateTime.parse(json['createdAt']),
+        updatedAt = DateTime.parse(json['updatedAt']),
+        title = json['title'],
+        body = json['body'],
+        rating = json['rating'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'title': title,
+      'body': body,
+      'rating': rating,
+    };
   }
 }
