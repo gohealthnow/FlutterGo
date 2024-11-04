1447c4598d76698f2c466bd79cd74e055fce6f9c
Polabiel <bielgabrieloliveira77@gmail.com>
2024-11-04 20:35:57 -0300

    Renomear método getChecklist para getSymptoms e adicionar método getDiagnosis para envio de sintomas

--- lib/api/repositories/expert_doctor_repository.dart
+++ lib/api/repositories/expert_doctor_repository.dart
@@ -1,6 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:gohealth/api/interfaces/user_interface.dart';
 import 'package:gohealth/api/services/http_client.dart';
 
 class expertDoctor {
@@ -13,13 +11,25 @@ class expertDoctor {
   // O expertDoctor é uma classe que irá representar a API da Inteligencia artificial que irá retornar os dados de cada função
 
   // ! Esta função recebe um prompt do usuário e a IA retorna um checklist de sintomas. O usuário deve marcar os sintomas que está sentindo, conforme o texto que ele escreveu e que gerou o checklist.
-  Future<Response> getChecklist(String prompt) async {
+  Future<Response> getSymptoms(String prompt) async {
     try {
       return await userNetworkClient.client.post(
-        '/checklist',
+        '/symptoms',
         data: {
           'prompt': prompt,
         },
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // ! Esta função irá enviar o checklist de sintomas marcados pelo usuário e a IA irá retornar um texto e uma descrição do que ele tem!
+  Future<Response> getDiagnosis(Map<String, dynamic> symptoms) async {
+    try {
+      return await userNetworkClient.client.post(
+        '/diagnosis',
+        data: symptoms,
       );
     } catch (e) {
       rethrow;
