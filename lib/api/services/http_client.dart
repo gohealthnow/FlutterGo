95608e3c6182b0a5fa84f5c1435d74ccb92b7d87
Polabiel <bielgabrieloliveira77@gmail.com>
2024-11-04 20:34:48 -0300

    Renomear construtor da classe HttpClient e adicionar classe MedicalExpertService para integração com API de IA

--- lib/api/services/http_client.dart
+++ lib/api/services/http_client.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class HttpClient {
   late Dio client;
 
-  HttpClient() {
+  HttpRequestHandler() {
     client = Dio(BaseOptions(
       baseUrl: dotenv.env['BASE_URL'] ?? 'http://10.0.2.2:3000',
       headers: {
@@ -33,5 +33,19 @@ class HttpClient {
       }
     }
     throw Exception('Failed to fetch data after $retries attempts');
+  }
+}
+
+
+class MedicalExpertService {
+  late Dio client;
+
+  HttpClient() {
+    client = Dio(BaseOptions(
+      baseUrl: dotenv.env['BASE_URL_IA'] ?? 'http://127.0.0.1:8000',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    ));
   }
 }
