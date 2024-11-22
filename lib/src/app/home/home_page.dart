ccfb99627b79552b4ba63a179472cb1537355491
Polabiel <bielgabrieloliveira77@gmail.com>
2024-11-22 09:59:49 -0300

    Adiciona funcionalidade de atualização com RefreshIndicator na página inicial

--- lib/src/app/home/home_page.dart
+++ lib/src/app/home/home_page.dart
@@ -13,69 +13,79 @@ class Homepage extends StatefulWidget {
 }
 
 class HomepageState extends State<Homepage> {
+  Future<void> _refresh() async {
+    await Future.delayed(
+        const Duration(seconds: 2)); // Simulação de uma atualização
+  }
+
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: const HeaderBarState(),
-      drawer: const SideMenu(),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  'Farmácias perto de você',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxHeight: screenHeight * 0.2, // 20% da altura da tela
-                ),
-                child: const PharmacyComponentState(),
-              ),
-              const Divider(),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  'Produtos perto de você',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxHeight: screenHeight * 0.2, // 60% da altura da tela
-                ),
-                child: const BannerComponent(hasPromotion: false),
-              ),
-              Divider(),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  'Produtos em promoção',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxHeight: screenHeight * 0.2, // 60% da altura da tela
-                ),
-                child: const BannerComponent(hasPromotion: true),
-              ),
-              ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxHeight: screenHeight * 0.2, // 20% da altura da tela
+        appBar: const HeaderBarState(),
+        drawer: const SideMenu(),
+        body: RefreshIndicator(
+          onRefresh: _refresh,
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Farmácias perto de você',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  child: const Divider()),
-              Expert(),
-            ],
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxHeight: screenHeight * 0.2, // 20% da altura da tela
+                    ),
+                    child: const PharmacyComponentState(),
+                  ),
+                  const Divider(),
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Produtos perto de você',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxHeight: screenHeight * 0.2, // 60% da altura da tela
+                    ),
+                    child: const BannerComponent(hasPromotion: false),
+                  ),
+                  Divider(),
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Produtos em promoção',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxHeight: screenHeight * 0.2, // 60% da altura da tela
+                    ),
+                    child: const BannerComponent(hasPromotion: true),
+                  ),
+                  ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: screenHeight * 0.2, // 20% da altura da tela
+                      ),
+                      child: const Divider()),
+                  Expert(),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
