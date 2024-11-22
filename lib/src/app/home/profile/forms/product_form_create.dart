af99eebbbbdcc8d1e18515333dcddb314cb28887
Polabiel <bielgabrieloliveira77@gmail.com>
2024-11-22 10:01:35 -0300

    Adiciona funcionalidade de seleção e upload de imagem no formulário de criação de produto

--- lib/src/app/home/profile/forms/product_form_create.dart
+++ lib/src/app/home/profile/forms/product_form_create.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gohealth/api/models/product_models.dart';
 import 'package:gohealth/api/repositories/product_repository.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ProductForm extends StatefulWidget {
   @override
@@ -8,6 +10,7 @@ class ProductForm extends StatefulWidget {
 }
 
 class _ProductFormState extends State<ProductForm> {
+  XFile? Imagem;
   final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _priceController = TextEditingController();
@@ -25,6 +28,7 @@ class _ProductFormState extends State<ProductForm> {
       final product = ProductModels(
         name: _nameController.text,
         price: double.parse(_priceController.text),
+        image: Imagem != null ? await saveImage() : null,
       );
 
       await _repository.createProduct(product).then((_) {
@@ -42,6 +46,33 @@ class _ProductFormState extends State<ProductForm> {
           ),
         );
       });
+    }
+  }
+
+  Future<String> saveImage() async {
+    final String path = 'images/${DateTime.now()}.png';
+    final File file = File(Imagem!.path);
+    await file.copy(path);
+    return path;
+  }
+
+  selecionarImagem() async {
+    final ImagePicker picker = ImagePicker();
+
+    try {
+      final XFile? imagemSelecionada = await picker.pickImage(
+        source: ImageSource.gallery,
+      );
+
+      if (imagemSelecionada != null) {
+        setState(() {
+          Imagem = imagemSelecionada;
+        });
+      }
+
+      print(Imagem);
+    } catch (e) {
+      print(e);
     }
   }
 
@@ -63,19 +94,69 @@ class _ProductFormState extends State<ProductForm> {
               return null;
             },
           ),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _priceController,
+                  decoration: InputDecoration(labelText: 'Preço do Produto'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira o preço do produto';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Por favor, insira um preço válido';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(width: 10),
+              GestureDetector(
+                onTap: () {
+                  selecionarImagem();
+                },
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[500],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Imagem != null
+                      ? Image.file(
+                          File(Imagem!.path),
+                          fit: BoxFit.cover,
+                        )
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.camera_alt,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Selecionar Imagem',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 10, height: 2),
+                            ),
+                          ],
+                        ),
+                ),
+              ),
+            ],
+          ),
           TextFormField(
-            controller: _priceController,
-            decoration: InputDecoration(labelText: 'Preço do Produto'),
-            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Descrição do Produto'),
             validator: (value) {
               if (value == null || value.isEmpty) {
-                return 'Por favor, insira o preço do produto';
-              }
-              if (double.tryParse(value) == null) {
-                return 'Por favor, insira um preço válido';
+                return 'Por favor, insira a descrição do produto';
               }
               return null;
             },
+            maxLines: 3,
           ),
           ElevatedButton(
             onPressed: _submitForm,
