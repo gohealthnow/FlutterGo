7020d07928a73fa3ae8d3690e425b311a3331994
Polabiel <bielgabrieloliveira77@gmail.com>
2024-11-29 22:06:37 -0300

    Add search functionality and navigation to Google Maps in MapsPage

--- lib/src/app/home/maps/maps_page.dart
+++ lib/src/app/home/maps/maps_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -9,8 +10,12 @@ import 'package:gohealth/src/components/header_bar.dart';
 import 'package:gohealth/src/components/side_menu.dart';
 import 'package:latlong2/latlong.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+
 class MapsPage extends StatefulWidget {
-  const MapsPage({Key? key}) : super(key: key);
+  final String? query;
+
+  const MapsPage({Key? key, this.query}) : super(key: key);
 
   @override
   _MapsPageState createState() => _MapsPageState();
@@ -25,6 +30,7 @@ class _MapsPageState extends State<MapsPage> {
   LatLng? _selectedPosition;
   LatLng? _mylocation;
   LatLng? _draggedPosition;
+  Marker? _searchMarker;
 
   Timer? _debounce;
 
@@ -92,29 +98,117 @@ class _MapsPageState extends State<MapsPage> {
       setState(() {
         _searchResults = data;
       });
+    
+      // If this was triggered by initial query parameter, move to first result
+      if (widget.query != null && widget.query == query) {
+        _moveToLocation(
+            double.parse(data[0]['lat']), double.parse(data[0]['lon']));
+        setState(() {
+          _isSearching = false;
+        });
+      }
     } else {
       setState(() {
         _searchResults = [];
       });
     }
   }
-
   // move to specific location
 
-  void _moveToLocation(double lat, double lng) {
+void _moveToLocation(double lat, double lng) {
     final location = LatLng(lat, lng);
     _mapController.move(location, 18.0);
+  
+    // Create new marker
     setState(() {
       _selectedPosition = location;
       _searchResults = [];
       _searchController.clear();
+    
+      // Remove previous search marker if exists
+      if (_searchMarker != null) {
+        _markers.remove(_searchMarker);
+      }
+
+      // Create new marker
+      _searchMarker = Marker(
+          point: location,
+          width: 40,
+          height: 40,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 5,
+                  offset: Offset(0, 2),
+                )
+              ],
+            ),
+            child: Icon(
+              Icons.location_on,
+              color: Colors.red,
+              size: 25,
+            ),
+          ));
+
+      // Add new marker to list
+      _markers.add(_searchMarker!);
     });
+  }
+
+  void _navigateToGoogleMaps() async {
+    if (_searchMarker != null) {
+      final lat = _searchMarker!.point.latitude;
+      final lng = _searchMarker!.point.longitude;
+
+      // For Android
+      final androidUrl = Uri.parse('google.navigation:q=$lat,$lng&mode=d');
+      // For iOS
+      final iosUrl = Uri.parse(
+          'comgooglemaps://?saddr=&daddr=$lat,$lng&directionsmode=driving');
+      // Fallback for web or if native apps not installed
+      final webUrl = Uri.parse(
+          'https://www.google.com/maps/dir/?api=1&destination=$lat,$lng');
+
+      try {
+        if (Platform.isAndroid && await canLaunchUrl(androidUrl)) {
+          await launchUrl(androidUrl);
+        } else if (Platform.isIOS && await canLaunchUrl(iosUrl)) {
+          await launchUrl(iosUrl);
+        } else if (await canLaunchUrl(webUrl)) {
+          await launchUrl(webUrl, mode: LaunchMode.externalApplication);
+        } else {
+          throw 'Could not launch maps';
+        }
+      } catch (e) {
+        debugPrint('Error launching maps: $e');
+        // Show error to user
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Erro ao abrir o Google Maps')),
+        );
+      }
+    }
   }
 
   @override
   void initState() {
     super.initState();
     _searchController.addListener(_onSearchChanged);
+    _showCurrentLocation();
+
+    if (widget.query != null && widget.query!.isNotEmpty) {
+      _searchController.text = widget.query!;
+      _searchPlaces(widget.query!);
+      // Reduce debounce time for initial search
+      _debounce = Timer(const Duration(milliseconds: 500), () {
+        _searchPlaces(widget.query!);
+      });
+    }
+
   }
 
   @override
@@ -142,6 +236,14 @@ class _MapsPageState extends State<MapsPage> {
           FlutterMap(
             options: MapOptions(
               initialZoom: 13.0,
+              initialCenter: _mylocation ?? LatLng(-23.5505, -46.6333),
+              minZoom: 2.0,
+              maxZoom: 18.0,
+              keepAlive: true,
+              interactionOptions: InteractionOptions(
+                flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+                scrollWheelVelocity: 0.005,
+              ),
               onTap: (tapPosition, point) => {
                 _selectedPosition = point,
                 _draggedPosition = _selectedPosition,
@@ -258,17 +360,32 @@ class _MapsPageState extends State<MapsPage> {
               ],
             ),
           ),
+
+// Update the Positioned FAB widget in build method
           Positioned(
-            bottom: 20,
+            bottom: 50,
             right: 20,
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 FloatingActionButton(
                   onPressed: () {
-                    _showCurrentLocation();
+                    _navigateToGoogleMaps();
                   },
-                  child: Icon(Icons.location_searching_rounded),
+                  heroTag:
+                      "locationButton", // Add unique tag to prevent hero animation conflicts
+                  child: const Icon(Icons.location_searching_rounded),
                 ),
+                if (_searchMarker != null) ...[
+                  const SizedBox(height: 10),
+                  FloatingActionButton(
+                    onPressed: _navigateToGoogleMaps,
+                    backgroundColor: const Color.fromARGB(255, 200, 252, 201),
+                    heroTag:
+                        "navigationButton", // Add unique tag to prevent hero animation conflicts
+                    child: const Icon(Icons.directions),
+                  ),
+                ]
               ],
             ),
           ),
